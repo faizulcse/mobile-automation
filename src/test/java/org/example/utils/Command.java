fb76bc2a03890b52aeb44ecda1f71ad644e49f25
faizul-bs23 <faizulcse@gmail.com>
2022-12-24 14:55:04 +0600

    update ios device find command

--- src/test/java/org/example/utils/Command.java
+++ src/test/java/org/example/utils/Command.java
@@ -5,5 +5,6 @@ public interface Command {
     String ATTACHED_DEVICES_MSG = "List of devices attached";
     String ANDROID_PLATFORM_VERSION = "adb -s %s shell getprop ro.build.version.release";
     String ANDROID_DEVICE_UDID = "adb -s %s shell 'settings get secure android_id'";
-    String IOS_DEVICE_COMMAND = "xcrun xctrace list devices | grep 'Booted'";
+    String IOS_DEVICE_COMMAND = "xcrun xctrace list devices";
+    String IOS_DEVICES_MSG = "== Devices ==";
 }
