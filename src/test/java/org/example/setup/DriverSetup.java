db5e34501f4de9033299eae3d707af92d162c212
faizul-bs23 <faizulcse@gmail.com>
2023-08-30 14:38:58 +0600

    fix driver issues

--- src/test/java/org/example/setup/DriverSetup.java
+++ src/test/java/org/example/setup/DriverSetup.java
@@ -1,21 +1,26 @@
 package org.example.setup;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
 import org.example.GlobalConfig;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
 import java.time.Duration;
 
 public class DriverSetup extends GlobalConfig {
-    private AppiumDriver driver;
+    public static AppiumDriver driver;
+    public static Dimension dimension;
+
 
     public void startDriver(String appiumUrl) {
         try {
             DesiredCapabilities caps = new CapsGenerator().getAppCapabilities(appType);
-            driver = isIos ? new IOSDriver(new URL(appiumUrl), caps) : new AppiumDriver(new URL(appiumUrl), caps);
+            driver = isIos ? new IOSDriver(new URL(appiumUrl), caps) : new AndroidDriver(new URL(appiumUrl), caps);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Integer.parseInt(appConfig.getString("IMPLICIT_WAIT"))));
+            dimension = driver.manage().window().getSize();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
